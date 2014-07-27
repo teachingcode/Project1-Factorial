050a711d468b4606dbf2d603a92b4eb1bff5b23b
Katherine Kuan <katherinekuan@katherinekuan-macbookair.roam.corp.google.com>
2014-07-27 00:57:02 -0700

    Add Blackjack project

---
+++ NewProject/src/Blackjack.java
@@ -0,0 +1,16 @@
+
+public class Blackjack {
+
+	public Blackjack() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+	}
+
+}
